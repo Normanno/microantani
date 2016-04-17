e9d0a3cfeb137600b964fc413669d67ee960d5c4
Normanno <vincenzonorman@gmail.com>
2016-04-17 11:27:06 +0200

    Update vincenzo_norman_vitale.py

--- vincenzo_norman_vitale.py
+++ vincenzo_norman_vitale.py
@@ -2,15 +2,15 @@ from microbit import *
 
 # vincenzonorman@gmail.com
 while true :
-	compass.calibrate()
-	while compass.is_calibrate() is False
-		compass.calibrate()
-	heading = compass.heading()
-	if(heading > 315 and  heading <= 45)
-		display.scroll("nord");
-	else if(heading > 45 and  heading <= 135)
-	    display.scroll("est");
-	if(heading > 135 and  heading <= 225)
-	   display.scroll("sud");
-	else if (heading > 225 and  heading <= 315)
-	   display.scroll("ovest")
+    microbit.compass.calibrate()
+    while microbit.compass.is_calibrate() is False
+        microbit.compass.calibrate()
+    heading = microbit.compass.heading()
+    if(heading > 315 and  heading <= 45)
+        microbit.display.scroll("nord");
+    else if(heading > 45 and  heading <= 135)
+        microbit.display.scroll("est");
+    if(heading > 135 and  heading <= 225)
+       microbit.display.scroll("sud");
+    else if (heading > 225 and  heading <= 315)
+       microbit.display.scroll("ovest")
